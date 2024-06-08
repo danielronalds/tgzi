d98df3165eb6a1e383e32e9772cfbe172ccbbadb
Daniel R <nzdanron@gmail.com>
2024-06-09 01:44:54 +1200

    feat: reading files in current directory

--- main.go
+++ main.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, world!");
+	files, err := getFiles(".", false)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, file := range files {
+		fmt.Println(file)
+	}
+}
+
+// Gets the list of files and directories in a given directory
+//
+// # Params
+//
+// - `dir` The directory to look in. i.e. "." looks in the current directory
+// - `hideDotfiles` Whether to return dotfiles or not
+//
+// # Returns
+//
+// Returns an error if the directory is unreadable, otherwise a slice of filenames as strings
+func getFiles(dir string, hideDotfiles bool) ([]string, error) {
+	entries, err := os.ReadDir(dir);
+	if err != nil {
+		return nil, err
+	}
+
+	files := make([]string, 0)
+
+	for _, entry  := range entries {
+		filename := entry.Name()
+
+		if filename[0] == '.' && hideDotfiles {
+			continue;
+		}
+
+		if entry.IsDir() {
+			filename = fmt.Sprintf("%v/", filename)
+		}
+		files = append(files, filename)
+	}
+
+	return files, nil
 }
